aec27d236a4c0f7a0cc080d351c794d55cb2c0cb
paulhilaire26000 <124454355+paulhilaire26000@users.noreply.github.com>
2024-05-15 13:44:36 +0200

    Restaurer '08/04/2024 19:01:51 Restaurer '29/03/2024 18:55:30 bauge bien''

--- main.ts
+++ main.ts
@@ -1,14 +1,38 @@
 mecanumRobotV2.initializeRobot()
-let vitesse = 100
+let vitesse = 50
+mecanumRobotV2.state()
 basic.forever(function () {
     if (irRemote.isIrButtonPressed(IrButton.Ok)) {
-        music.play(music.stringPlayable("C5 B A G F E D C ", 120), music.PlaybackMode.UntilDone)
-        basic.showString("j'avence")
+        mecanumRobotV2.state()
+    }
+    if (irRemote.isIrButtonPressed(IrButton.Up)) {
         mecanumRobotV2.Motor(LR.Upper_left, MD.Forward, vitesse)
         mecanumRobotV2.Motor(LR.Lower_left, MD.Forward, vitesse)
         mecanumRobotV2.Motor(LR.Upper_right, MD.Forward, vitesse)
         mecanumRobotV2.Motor(LR.Lower_right, MD.Forward, vitesse)
-        basic.pause(5000)
-        mecanumRobotV2.state()
+    }
+    if (irRemote.isIrButtonPressed(IrButton.Down)) {
+        mecanumRobotV2.Motor(LR.Upper_left, MD.Back, vitesse)
+        mecanumRobotV2.Motor(LR.Lower_left, MD.Back, vitesse)
+        mecanumRobotV2.Motor(LR.Upper_right, MD.Back, vitesse)
+        mecanumRobotV2.Motor(LR.Lower_right, MD.Back, vitesse)
+    }
+    if (irRemote.isIrButtonPressed(IrButton.Left)) {
+        mecanumRobotV2.Motor(LR.Upper_left, MD.Back, vitesse)
+        mecanumRobotV2.Motor(LR.Lower_left, MD.Forward, vitesse)
+        mecanumRobotV2.Motor(LR.Upper_right, MD.Forward, vitesse)
+        mecanumRobotV2.Motor(LR.Lower_right, MD.Back, vitesse)
+    }
+    if (irRemote.isIrButtonPressed(IrButton.Right)) {
+        mecanumRobotV2.Motor(LR.Upper_left, MD.Forward, vitesse)
+        mecanumRobotV2.Motor(LR.Lower_left, MD.Back, vitesse)
+        mecanumRobotV2.Motor(LR.Upper_right, MD.Back, vitesse)
+        mecanumRobotV2.Motor(LR.Lower_right, MD.Forward, vitesse)
+    }
+    if (irRemote.isIrButtonPressed(IrButton.Star)) {
+        vitesse += -5
+    }
+    if (irRemote.isIrButtonPressed(IrButton.Hash)) {
+        vitesse += 5
     }
 })
